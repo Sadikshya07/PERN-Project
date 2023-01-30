c15f5f1619a0cf2d98a916742dcff51adab66092
Aashish Kadel <kadelaashish22@gmail.com>
2023-01-30 19:08:21 +0545

    card changes and react slick

--- client/components/TermandAnalysisCard.jsx
+++ client/components/TermandAnalysisCard.jsx
@@ -2,12 +2,25 @@ import Image from "next/image";
 import Head from "next/head";
 import Link from "next/link";
 
-export default function TermAndAnalysisReport(){
-    return (
-        <div className="card">
-            <div className="image"></div>
-            <div className="title">Title</div>
-            <Link href = "/">VIEW</Link>
+export default function TermAndAnalysisReport() {
+  return (
+    <div className="card">
+      <div className="card-content bg-orange relative w-[18em] h-[24em] rounded-xl">
+        <div
+          className="image w-full h-9/12"
+          style={{
+            backgroundImage:
+              "url(https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_4x3.jpg)",
+            backgroundSize: "cover",
+          }}
+        ></div>
+        <div className="bottom-content w-full absolute bottom-4 text-center">
+          <div className="title w-11/12 mx-auto font-semibold">ncjsdnc nsjdknc ncksjn cnduj ncjd cnjd cjdb</div>
+          <div className="view-button mt-6 font-bold">
+            <Link href="/">VIEW</Link>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -238,8 +238,8 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="school-activities mt-2 bg-gray">
-          <div className="school-activities-content w-11/12 mx-auto py-8">
+        <div className="school-activities mt-2 bg-gray py-8">
+          <div className="school-activities-content w-11/12 mx-auto">
             <h1 className="text-4xl font-bold mb-6">School Activities</h1>
             <div className="embeds flex gap-10 justify-between">
               <div className="school-activities-card  bg-offWhite w-[20em] h-[20em]">
@@ -282,26 +282,26 @@ export default function Home() {
                   className="w-full h-full rounded-xl"
                 ></iframe>
               </div>
-              <div className="school-activities-card  bg-offWhite w-[20em] h-[20em]">
-                <iframe
-                  src="https://www.youtube.com/embed/_YGI1qRd9pw"
-                  title="YouTube video player"
-                  frameborder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                  allowfullscreen
-                  className="w-full h-full rounded-xl"
-                ></iframe>
-              </div>
-              <div className="school-activities-card  bg-offWhite w-[20em] h-[20em]">
-                <iframe
-                  src="https://www.youtube.com/embed/_YGI1qRd9pw"
-                  title="YouTube video player"
-                  frameborder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                  allowfullscreen
-                  className="w-full h-full rounded-xl"
-                ></iframe>
-              </div>
+              {/* <div className="school-activities-card  bg-offWhite w-[20em] h-[20em]">
+              <iframe
+                src="https://www.youtube.com/embed/_YGI1qRd9pw"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen
+                className="w-full h-full rounded-xl"
+              ></iframe>
+            </div>
+            <div className="school-activities-card  bg-offWhite w-[20em] h-[20em]">
+              <iframe
+                src="https://www.youtube.com/embed/_YGI1qRd9pw"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen
+                className="w-full h-full rounded-xl"
+              ></iframe>
+            </div> */}
             </div>
           </div>
         </div>
