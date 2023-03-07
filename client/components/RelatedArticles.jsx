13bf760bed1389deb5b4b435e5cf08685d179b68
Kritartha Sapkota <kritarthasapkota999@gmail.com>
2023-03-07 19:03:43 +0545

    added link to other articles

--- client/components/RelatedArticles.jsx
+++ client/components/RelatedArticles.jsx
@@ -1,17 +1,21 @@
+import Link from "next/link";
+
 const RelatedArticles = ({ otherArticle }) => {
   const otherarticles = otherArticle.map((article) => {
     return (
-      <div className="other-posts-card" key={article.id}>
-        <div
-          className="card w-[18rem] h-[18rem] bg-orange rounded-xl mb-4 object-fill"
-          style={{
-            background: `url('http://${article.image1}')`,
-          }}
-        ></div>
-        <p className="text-orange font-semibold text-2xl w-[18rem]">
-          {article.title}
-        </p>
-      </div>
+      <Link href={`/news-and-events/${article.id}`}>
+        <div className="other-posts-card" key={article.id}>
+          <div
+            className="card w-[18rem] h-[18rem] bg-orange rounded-xl mb-4 object-fill"
+            style={{
+              background: `url('http://${article.image1}')`,
+            }}
+          ></div>
+          <p className="text-orange font-semibold text-2xl w-[18rem]">
+            {article.title}
+          </p>
+        </div>
+      </Link>
     );
   });
 
