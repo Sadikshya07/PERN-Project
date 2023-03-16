6569fc6838bff8143ee17a14e4b9d11086b11277
Justaway <kritarthasapkota999@gmail.com>
2023-03-16 10:21:11 +0545

    management card added image

--- client/components/ManagementCard.jsx
+++ client/components/ManagementCard.jsx
@@ -7,15 +7,15 @@ export default function ManagementCard({ name, description, position }) {
         <div
           className="image-container w-full h-[20em] md:h-[30em] mx-auto rounded-3xl"
           style={{
-            backgroundImage:
-              "url(https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_4x3.jpg)",
+            backgroundImage: `url('${process.env.NEXT_PUBLIC_SERVER_HOST}${image}')`,
             backgroundSize: "cover",
           }}
-        >
-        </div>
+        ></div>
       </div>
       <div className="info text-center">
-        <h1 className="mt-5 text-2xl md:text-4xl font-bold text-orange">{name}</h1>
+        <h1 className="mt-5 text-2xl md:text-4xl font-bold text-orange">
+          {name}
+        </h1>
         <p className="text-lg md:text-xl font-semibold">{position}</p>
       </div>
       <p className="description mx-auto my-4 md:w-[37.5rem] px-6 text-center text-lg md:text-xl">
